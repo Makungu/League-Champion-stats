b41efa7347cd3df98812ffa9f73692012b745351
Makungu <makungu.c200@gmail.com>
2021-06-21 15:28:49 +0200

    added features for futher data management

--- Controllers/ManageChampionsController.cs
+++ Controllers/ManageChampionsController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using leagueChampionStats.DataAccessLayer;
 using leagueChampionStats.Models;
 using System;
+using System.Data.Entity;
+using System.Linq;
+//using Microsoft.EntityFrameworkCore.Relational;
 
 
 namespace leagueChampionStats.Controllers
@@ -17,30 +20,39 @@ namespace leagueChampionStats.Controllers
 
         public IActionResult SubmitAdd(string name, string role, string type, string region)
         {
-            
-            champion champ = new champion()
-            {
-                name = name,
-                role = role,
-                type = type,
-                region = region
-            };
-            context.champions.Add(champ);
-            context.SaveChanges();
+            champion champResult = context.champions.FirstOrDefault(r => r.name == name);
+           if (champResult == null)
+           {
+                champion champ = new champion()
+                {
+                    name = name,
+                    role = role,
+                    type = type,
+                    region = region
+                }; 
+                context.champions.Add(champ);
+                context.SaveChanges();
+           }
             return View("AddChampion");
         }
 
-        public IActionResult submitEdit()
+        public IActionResult submitEdit(string name, string role, string type, string region)
         {
-            foreach (champion c in context.champions)
-            {
-                
-            }
+            
+            champion champ = context.champions.FirstOrDefault(r => r.name == name);
+            champ.name =name;
+            champ.role = role;
+            champ.type = type;
+            champ.region = region;
+            context.SaveChanges();
             return View("EditChampion");
         }
 
-        public IActionResult submitRemove()
+        public IActionResult submitRemove(string name)
         {
+            champion champ = context.champions.FirstOrDefault(r => r.name == name);
+            context.champions.Remove(champ);
+            context.SaveChanges();
             return View("RemoveChampion");
         }
 
